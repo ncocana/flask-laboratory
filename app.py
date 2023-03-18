586bd864ac5130d1f3dd3c808055e187aa38d9e5
ncocana <ncocana@cifpfbmoll.eu>
2023-03-18 21:18:22 +0100

    feat(app, templates): add layout.html

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 # Turns this file into a Flask application.
 app = Flask(__name__)
 
+# In Python, this is called a "decorator".
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -10,4 +11,6 @@ def index():
 @app.route('/greet')
 def greet():
     name = request.args.get("name", "World")
+    if name == "":
+        name = "World"
     return render_template("greet.html", name = name)
