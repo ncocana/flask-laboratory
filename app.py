5262eb3a020d1514380952a7448a43a7325d03a6
ncocana <ncocana@cifpfbmoll.eu>
2023-03-18 23:47:50 +0100

    feat(app, templates): add security and for loop to sports

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import Flask, render_template, request
 # Turns this file into a Flask application.
 app = Flask(__name__)
 
+REGISTRANTS = {}
+
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Frisbee"
+]
+
 # In Python, this is called a "decorator".
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -11,17 +19,22 @@ def index():
     if request.method == "POST":
         # "something?key=value" is called Query parameters/HTTP parameters.
         name = request.form.get("name", "World")
-        if name == "":
-            name = "World"
+        if not name:
+            return render_template("register-failure.html")
         return render_template("greet.html", name = name)
 
 @app.route("/sports", methods=["GET", "POST"])
 def sports():
     if request.method == "GET":
-        return render_template("sports.html")
+        return render_template("sports.html", sports=SPORTS)
     if request.method == "POST":
         name = request.form.get("name", "World")
         sport = request.form.get("sport", "No sport selected")
-        if name == "":
-            name = "World"
+        if sport not in SPORTS or not name:
+            return render_template("register-failure.html")
+        REGISTRANTS[name] = sport
         return render_template("register-success.html", name = name, sport = sport)
+
+@app.route("/sports/registrants")
+def registrants():
+    return render_template("registrants.html", registrants = REGISTRANTS)
